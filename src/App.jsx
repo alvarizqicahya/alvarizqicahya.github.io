f0be8a86315d7222974ad7c3a9dec1f7dd3050e5
alvarizqicahya <alvarizqicahya@gmail.com>
2024-12-15 13:50:51 +0700

    feat: add landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import React from 'react';
+import Attribute from "./Attribute.jsx";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+            <div className="text-center px-6 sm:px-12 lg:px-24">
+                <div className="mb-6 relative w-48 h-48 mx-auto">
+                    <img
+                        src={Attribute.ImgProfile || 'https://via.placeholder.com/150'}
+                        alt="Profile"
+                        loading="lazy"
+                        className="absolute rounded-full"
+                    />
+                    <div className="absolute w-48 h-48 rounded-full border-b-2 border-teal-500 animate-spin"/>
+                </div>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+                <h1 className="text-4xl font-semibold mb-2">{Attribute.Name}</h1>
+
+                <p className="text-lg text-gray-400 mb-4">{Attribute.JobTitle}</p>
+
+                <div className="flex justify-center space-x-6">
+                    <a href={Attribute.SocialGithub} target="_blank" rel="noopener noreferrer"
+                       className="text-teal-500 hover:text-teal-400">
+                        <i className="fab fa-github text-2xl"></i>
+                    </a>
+                    <a href={Attribute.SocialLinkedin} target="_blank" rel="noopener noreferrer"
+                       className="text-teal-500 hover:text-teal-400">
+                        <i className="fab fa-linkedin text-2xl"></i>
+                    </a>
+                    <a href={Attribute.SocialTelegram} target="_blank" rel="noopener noreferrer"
+                       className="text-teal-500 hover:text-teal-400">
+                        <i className="fab fa-telegram text-2xl"></i>
+                    </a>
+                    <a href={Attribute.SocialEmail} target="_blank" rel="noopener noreferrer"
+                       className="text-teal-500 hover:text-teal-400">
+                        <i className="fa fa-square-envelope text-2xl"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
---
+++ src/Attribute.jsx
@@ -0,0 +1,15 @@
+import profilePict from './assets/profile.png'
+import imageIcon from './assets/icon.png'
+
+const Attribute = {
+    'Name': 'Alvarizqi Cahya Saputra',
+    'ImgProfile': profilePict,
+    'ImgIcon': imageIcon,
+    'JobTitle': 'Software Developer',
+    'SocialGithub': 'https://github.com/alvarizqicahya',
+    'SocialLinkedin': 'https://www.linkedin.com/in/alvarizqicahya/',
+    'SocialTelegram': 'https://t.me/alvarizqicahya',
+    'SocialEmail': 'mailto:alvarizqicahya@gmail.com',
+}
+
+export default Attribute;
